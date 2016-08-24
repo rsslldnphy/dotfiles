5a6398aa2a9517b54ae685f1cb08ffc37afa26e0
Russell <me@rsslldnphy.com>
2016-08-24 22:28:17 +0100

    whitespace

--- setup.sh
+++ setup.sh
@@ -6,7 +6,6 @@ brew install leiningen nginx rbenv sl tmux tree     2> /dev/null
 brew install macvim    --with-override-system-vim   2> /dev/null
 brew install coreutils --with-default-names         2> /dev/null
 
-
 echo "*** Installing essential gems"
 sudo gem install lolcat
 
