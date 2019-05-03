29f5b23e4ffdc526a1c0424c6ea079e24127af3e
Russell Dunphy <russell@russelldunphy.com>
2019-05-03 12:54:41 +0100

    remove deprecated options; switch nvm to n

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,7 @@
 set fish_greeting ''
-set -gx PATH ~/.bin ./node_modules/.bin $PATH
+
+set -gx N_PREFIX ~/.n
+set -gx PATH ~/.bin ./node_modules/.bin /usr/local/opt/coreutils/libexec/gnubin $N_PREFIX/bin $PATH
 set -gx CDPATH ~ ~/Code .
 
 source ~/.config/fish/aliases.fish
-source ~/.config/fish/ssh-agent.fish
-
-status --is-interactive; and source (rbenv init -|psub)
