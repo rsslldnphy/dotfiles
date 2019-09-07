b537c4fe6b52281f735eebed581470ab81418d26
Russell Dunphy <russell@russelldunphy.com>
2019-09-07 17:02:37 +0100

    Run ssh-add once only

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,7 @@
 set fish_greeting ''
 
+ssh-add -l |grep -q (ssh-keygen -lf ~/.ssh/id_rsa  | awk '{print $2}') || ssh-add ~/.ssh/id_rsa
+
 set -gx N_PREFIX ~/.n
 set -gx PATH ~/.bin ./node_modules/.bin /usr/local/opt/coreutils/libexec/gnubin $N_PREFIX/bin $PATH
 set -gx CDPATH ~ ~/Code ~/Code/caseworker .
