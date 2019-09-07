9cac08ed5430edba8697b9df3b99229c06a2b85a
Russell Dunphy <russell@russelldunphy.com>
2019-09-07 17:33:41 +0100

    Add solaris to cdpath

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,7 @@ ssh-add -l |grep -q (ssh-keygen -lf ~/.ssh/id_rsa  | awk '{print $2}') || ssh-ad
 
 set -gx N_PREFIX ~/.n
 set -gx PATH ~/.bin ./node_modules/.bin /usr/local/opt/coreutils/libexec/gnubin $N_PREFIX/bin $PATH
-set -gx CDPATH ~ ~/Code ~/Code/caseworker .
+set -gx CDPATH ~ ~/Code ~/Code/caseworker ~/Code/solaris .
 set -gx GOPATH ~/.go
 
 source ~/.config/fish/aliases.fish
