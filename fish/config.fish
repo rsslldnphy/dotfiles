a97ed973299858685e81374f505e615e5839f9b0
Russell Dunphy <russell@russelldunphy.com>
2024-04-03 20:10:59 +0100

    move abbreviations into main fish config

--- fish/config.fish
+++ fish/config.fish
@@ -8,6 +8,24 @@ set -gx GPG_TTY (tty)
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
 
 # aliases
-alias e nvim
 
-starship init fish | source
+
+if status is-interactive
+    starship init fish | source
+
+    alias e nvim
+    alias p pnpm
+
+    abbr :q exit
+    abbr gb gh repo view --web
+    abbr gci git commit -m
+    abbr gco git checkout
+    abbr gl git pull
+    abbr glog git log --pretty=format:"'%C(auto)%h - %<(80,trunc)%s %Cgreen(%cr) %C(bold blue)<%an>%Creset'" --abbrev-commit
+    abbr gp git push
+    abbr gpf git push --force-with-lease
+    abbr gpo git push -u origin (git rev-parse --abbrev-ref HEAD)
+    abbr gst git status
+    abbr gti git
+    abbr p pnpm
+end
